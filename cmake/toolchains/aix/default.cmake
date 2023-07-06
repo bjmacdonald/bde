e5ce6a64a24b63885164aecbe71de0d1b17d1559
Bruce MacDonald <bruce@bkmacd.com>
2023-07-07 00:19:25 +0100

    added cmake files from bde-tools; cmakepresets.json

---
+++ cmake/toolchains/aix/default.cmake
@@ -0,0 +1,1 @@
+xlc-default.cmake
---
+++ cmake/toolchains/aix/xlc-12.1.cmake
@@ -0,0 +1,5 @@
+set(root /bb/util/version12-082017/usr/vacpp/bin)
+set(CMAKE_C_COMPILER ${root}/xlc_r)
+set(CMAKE_CXX_COMPILER ${root}/xlC_r)
+
+include("toolchains/aix/xlc_default")
